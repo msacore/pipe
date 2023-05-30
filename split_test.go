2f2d0793b2410159cf74bfd54484d04aa870c308
Yuri Kulagin <yvkulagin@sberbank.ru>
2023-05-30 16:30:00 +0300

    extra code commented/removed
    Signed-off-by: Yuri Kulagin <yvkulagin@sberbank.ru>

--- split_test.go
+++ split_test.go
@@ -1,8 +1,6 @@
 package pipe
 
 import (
-	"errors"
-	"sync"
 	"testing"
 
 	"github.com/msacore/pipe/test"
@@ -19,50 +17,6 @@ func TestSplit(t *testing.T) {
 
 			return pipes, epipe
 		})
-	})
-
-	t.Run("Sync", func(t *testing.T) {
-		const channelCapacity = 64
-		const dataCount = channelCapacity * 8
-		const outputsCount = 16
-
-		input := make(chan int, channelCapacity)
-		go func() {
-			for i := 0; i < dataCount; i++ {
-				input <- i
-			}
-			close(input)
-		}()
-
-		outs := SplitSync(outputsCount, input)
-		errs := make([]error, outputsCount)
-
-		wg := sync.WaitGroup{}
-		for i := 0; i < outputsCount; i++ {
-			i := i
-			wg.Add(1)
-			go func() {
-				max := -1
-				for val := range outs[i] {
-					if val > max {
-						max = val
-					} else {
-						errs[i] = errors.New("the data order is broken")
-					}
-				}
-				if max < dataCount-1 {
-					errs[i] = errors.New("not all data has been processed")
-				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-
-		for _, err := range errs {
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
 	})
 
 	t.Run("Sync", func(t *testing.T) {
